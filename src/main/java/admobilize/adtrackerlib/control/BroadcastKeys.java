bc93a55d56a6803b7c96dd703107fde2323e96e6
Antonio Vanegas <hpsaturn@gmail.com>
2018-12-12 23:17:50 +0100

    fixed package constants access for library

--- src/main/java/admobilize/adtrackerlib/control/BroadcastKeys.java
+++ src/main/java/admobilize/adtrackerlib/control/BroadcastKeys.java
@@ -6,14 +6,13 @@ package admobilize.adtrackerlib.control;
 
 class BroadcastKeys {
 
-
-    public static final String KEY_DETECTOR_ID   = "key_adm_detector_id";
-    public static final String CAMERA_NAME = "key_camera_name";
-    public static final String CAMERA_URL = "key_camera_url";
-    public static final String CAMERA_ENABLE = "key_camera_enable";
-    public static final String CAMERA_PREVIEW = "key_camera_preview";
-    public static final String SERVICE_MSG = "key_camera_service_msg";
-    public static final String CAMERA_ORIENTATION = "key_camera_orientation";
-    public static final String CAMERA_ORIENTATION_ALLOWED = "key_camera_orientation_allowed";
+    static final String KEY_DETECTOR_ID   = "key_adm_detector_id";
+    static final String CAMERA_NAME = "key_camera_name";
+    static final String CAMERA_URL = "key_camera_url";
+    static final String CAMERA_ENABLE = "key_camera_enable";
+    static final String CAMERA_PREVIEW = "key_camera_preview";
+    static final String SERVICE_MSG = "key_camera_service_msg";
+    static final String CAMERA_ORIENTATION = "key_camera_orientation";
+    static final String CAMERA_ORIENTATION_ALLOWED = "key_camera_orientation_allowed";
 
 }
